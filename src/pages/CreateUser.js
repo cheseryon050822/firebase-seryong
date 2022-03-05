34f0b099b955402a1ac613c1594ad2ea8bf911e8
cheseryon050822 <cseryon@gmail.com>
2022-03-05 22:40:08 +0900

    ukline

--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -1,35 +1,37 @@
 import React,{useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link,useHistory} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { TextField,Button } from "@material-ui/core";
 
 const CreateUser=()=>{
 const [email,setEmail]=useState('')
 const [password,setPassword]=useState('')
 const [error,setError]=useState('')
-// const history=useHistory();
+let navigate = useNavigate()
 
-const handleClick = async () =>{
+const handleClick = async() =>{
     const result =await CreateUser(email,password)
-    // console.log("~file:CreateUser.js ~ line 13 ~ handleClick ~ result",result)
-    // if (result){
-    //     history.push('/main')
-    // }else{
-    //     setError("User作成に失敗しました")
-    // }
+    console.log("~file:CreateUser.js ~ line 13 ~ handleClick ~ result",result)
+     if (result){
+         navigate("/main")
+     }else  {
+         setError("User作成に失敗しました")
+      }
 }
  return(
 <div>
-    <h1>createuser</h1>
+    <h2>Createuser</h2>
     <div>
     <Link to="/login">login</Link>
     </div>
     <div>
     <Link to="/main">main</Link>
     </div>
+    <div>
     <TextField id="email" label="email" value={email} onChange={e=>setEmail(e.target.value)}></TextField><br/>
     <TextField id="password" label="password" value={password} onChange={e=>setPassword(e.target.value)}></TextField><br/>
-    <Button onClick={handleClick}></Button>
+    <button onClick={handleClick}>新井が悪い</button>
+    </div>
 </div>
  );   
 }
