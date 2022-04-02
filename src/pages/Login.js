a800564c729dd2d8ca63804b522f2edbe39cbd5d
cheseryon050822 <cseryon@gmail.com>
2022-04-02 19:00:53 +0900

    sonic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link,useHistory } from 'react-router-dom';
+import { Link,useNavigate} from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { TextField,Button } from "@material-ui/core";
 
 const Login=()=>{
+const [email,setEmail]=useState('')
+const [password,setPassword]=useState('')
+const [error,setError]=useState('')
+const navigate = useNavigate()
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode)
+    console.log(errorMessage)
+  });
 
  return(
 <div>
@@ -12,6 +32,11 @@ const Login=()=>{
     </div>
     <div>
     <Link to="/main">main</Link>
+    </div>
+    <div>
+    <TextField id="email" label="email" value={email} onChange={e=>setEmail(e.target.value)}></TextField><br/>
+    <TextField id="password" label="password" value={password} onChange={e=>setPassword(e.target.value)}></TextField><br/>
+    <Button onClick={auth}>古木あーっと！</Button>
     </div>
 </div>
  );   
